7d1fcddad507f672034b3333cc0e43cf1b47fe17
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-13 10:28:04 -0500

    random colors at random locations

--- cubes.py
+++ cubes.py
@@ -1,11 +1,14 @@
 import turtle
+import random
 
 wn = turtle.Screen()
 t = turtle.Turtle()
 
+colors = ['red', 'yellow', 'green', 'blue', 'hotpink']
+
 t.up()
-t.goto(-50, 100)
-t.color('yellow')
+t.goto(random.randrange(-50, 100), random.randrange(-50, 100))
+t.color(random.choice(colors))
 for _ in range(5):
     t.forward(50)
     t.left(90)
