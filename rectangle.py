cdb6499d738ced4dda3226ff50b3f391a15b59ff
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-13 10:34:44 -0500

    keep drawing lines until the end of rectangle - 100 units

--- rectangle.py
+++ rectangle.py
@@ -23,16 +23,19 @@ for distance in [400, 300, 400, 300]:
     jan.left(90)
 
 # start drawing lines inside with spacing of 10 units
-for distance in [10, 100]:
-    jan.forward(distance)
-    jan.left(90)
+for _ in range(20):
+    '''using range 20 because we are doing two tasks within the main loop each with a distance of 10 units so
+    range(20)*2(tasks)*10(units)'''
+    for distance in [10, 100]:
+        jan.forward(distance)
+        jan.left(90)
 
-jan.right(180)
+    jan.right(180)
 
-for distance in [10, 100]:
-    jan.forward(distance)
-    jan.right(90)
+    for distance in [10, 100]:
+        jan.forward(distance)
+        jan.right(90)
 
-jan.right(180)
+    jan.right(180)
 
 wn.exitonclick()
